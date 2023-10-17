fe0fcf4d5be999217cc58b1a65499a0bdba8a8bd
Jéhèndé <kylian2004@free.fr>
2023-10-17 09:10:52 +0200

    Add files via upload

--- server ftp/index.js
+++ server ftp/index.js
@@ -7,18 +7,80 @@ const { io } = require("socket.io-client");
 const SocketUrl = config.socket.url;
 const socket = io(SocketUrl);
 
-const FtpSrv = require("ftp-srv");
+const FtpSrv = require("ftpd");
 const FtpPort= config.ftp.port;
 const FtpPath= config.ftp.path;
 const FtpLoginTimeout= config.ftp.loginTimeout;
 
 
 
-const ftpServer = new FtpSrv({
-	url: "ftp://0.0.0.0:" + FtpPort,
-	anonymous: true
+const ftpServer = new FtpSrv.FtpServer("0.0.0.0", {
+	getInitialCwd: function() {
+		return "/";
+	},
+	getRoot: function() {
+		return FtpPath;
+	},
+	pasvPortRangeStart: 1025,
+	pasvPortRangeEnd: 1050,
+	tlsOptions: false,
+	allowUnauthorizedTls: true,
+	useWriteFile: false,
+	useReadFile: false,
+	uploadMaxSlurpSize: 7000, // N/A unless 'useWriteFile' is true.
 });
 
+
+ftpServer.on("error", function(error) {
+	console.log("FTP Server error:", error);
+});
+ftpServer.on("client:connected", function(connection) {
+
+	let username = null;
+	console.log("client connected: " + connection.remoteAddress);
+	connection.on("command:user", function(user, success, failure) {
+		if (user) {
+			username = user;
+			success();
+		} else {
+			failure();
+		}
+	});
+  
+	connection.on("command:pass", function(pass, success, failure) {
+		if (pass) {
+			let timedout = setTimeout(()=> {
+				timedout = undefined;
+				return failure("Login Timed Out");
+			}, FtpLoginTimeout);
+
+			socket.emit("login", username, pass, callback => {
+				clearTimeout(timedout);
+				if (timedout === undefined) return;
+				if (callback.result === true){
+					return success(username);    
+				}
+				return failure("Invalid username or password");
+			});
+			
+		} else {
+			failure();
+		}
+	});
+});
+
+
+
+ftpServer.debugging = 4;
+ftpServer.listen(FtpPort);
+console.log("Listening on port " + FtpPort);
+
+/*new FtpSrv({
+	url: "ftp://0.0.0.0:" + FtpPort,
+	anonymous: true
+});*/
+
+/*
 ftpServer.on("login", ({connection, username, password}, resolve, reject)=> {
 	let timedout = setTimeout(()=> {
 		timedout = undefined;
@@ -39,3 +101,4 @@ ftpServer.on("login", ({connection, username, password}, resolve, reject)=> {
 ftpServer.listen().then(() => { 
 	console.log("Ftp server is starting...");
 });
+*/
